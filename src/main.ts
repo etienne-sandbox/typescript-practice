cb8ba87d28c0dc75d29c10fc75da42379042009d
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2022-01-20 14:48:31 +0100

    Fetch workouts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,50 @@
-import './style.css'
+import "./style.css";
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const root = notNil(document.getElementById("app"));
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+type Workout = {
+  id: string;
+  date: string;
+  place: string;
+  distance: number;
+  duration: number;
+  user: string;
+  placeName: string;
+  speed: number;
+  userName: string;
+};
+
+type WorkoutResponse = {
+  results: Array<Workout>;
+  count: number;
+};
+
+const workoutsDisplay = createDisplay();
+
+fetch("http://localhost:3001/workouts")
+  .then((res) => res.json())
+  .then((data: WorkoutResponse) => {
+    workoutsDisplay(
+      data.results
+        .map(
+          (workout) =>
+            `${workout.date} - ${workout.distance}m ${workout.duration}mn`
+        )
+        .join("\n")
+    );
+  });
+
+function createDisplay() {
+  const elem = document.createElement("pre");
+  root.appendChild(elem);
+  return (content: string) => {
+    elem.innerHTML = content;
+  };
+}
+
+function notNil<T>(val: T | null | undefined): T {
+  if (val === null || val === undefined) {
+    throw new Error("Unexpected null or undefined");
+  }
+  return val;
+}
