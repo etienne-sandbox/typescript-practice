f7daaa42207147e7afd7b97da33d0cd3854045c6
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2022-02-15 14:45:17 +0100

    Define types

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,39 @@
 export {};
 
-// User
-// Author
-// Admin
+type PermissionItem = false | "read" | "read-write";
+
+type Permissions = {
+  comments: PermissionItem;
+  articles: PermissionItem;
+  users: PermissionItem;
+};
+
+type Comment = {
+  userId: string;
+  content: string;
+};
+
+type Article = {
+  id: string;
+  title: string;
+  content: string;
+  comments: Array<Comment>;
+};
+
+type Reader = {
+  role: "Reader";
+  name: string;
+  age: number;
+  permissions: Permissions;
+  favArtciles: Array<string>;
+};
+
+type Author = {
+  role: "Author";
+  name: string;
+  age: number;
+  permissions: Permissions;
+  articles: Array<Article>;
+};
+
+type User = Author | Reader;
