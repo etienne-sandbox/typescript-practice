ca6da2c55b66ff540040c1e8daa809ef347597a4
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2022-05-04 15:31:59 +0200

    Implement useFetch

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,62 @@
 import { useFetch } from "../hooks/useFetch";
 
-export function App() {
-  const places = useFetch("http://localhost:3001/places");
-  const workouts = useFetch("http://localhost:3001/workouts");
+type Place = {
+  image: string;
+  name: string;
+  slug: string;
+  workoutCount: number;
+};
 
-  return <div className="App">Hello React</div>;
+type PlacesResponse = {
+  results: Place[];
+  total: number;
+};
+
+type Workout = {
+  id: string;
+  date: string;
+  place: string;
+  distance: number;
+  duration: number;
+  user: string;
+  placeName: string;
+  speed: number;
+  userName: string;
+};
+
+type WorkoutsResponse = {
+  results: Workout[];
+  total: number;
+};
+
+export function App() {
+  const places = useFetch<PlacesResponse>("http://localhost:3001/places");
+  const workouts = useFetch<WorkoutsResponse>("http://localhost:3001/workouts");
+
+  return (
+    <div className="App">
+      <div>
+        <h2>Places</h2>
+        {places === null ? (
+          <p>Loading...</p>
+        ) : (
+          places.results.map((place) => (
+            <div key={place.slug}>{place.name}</div>
+          ))
+        )}
+      </div>
+      <div>
+        <h2>Workouts</h2>
+        {workouts === null ? (
+          <p>Loading...</p>
+        ) : (
+          workouts.results.map((workout) => (
+            <div key={workout.id}>
+              {workout.distance}m - {workout.duration}min
+            </div>
+          ))
+        )}
+      </div>
+    </div>
+  );
 }
--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,3 +1,23 @@
-export function useFetch() {
-  throw new Error("Method not implemented.");
+import { useState, useEffect } from "react";
+
+export function useFetch<Data>(url: string): Data | null {
+  const [data, setData] = useState<Data | null>(null);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    (async () => {
+      try {
+        const res = await fetch(url, { signal: controller.signal });
+        const data = await res.json();
+        setData(data);
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+
+    return () => controller.abort();
+  }, []);
+
+  return data;
 }
